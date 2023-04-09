30643f5395b8f33c44defa4198005f3d9363a694
ChichaDimas <vilkamil9@gmail.com>
2023-04-09 12:46:21 +0300

    django-poc

--- project/main/views.py
+++ project/main/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .forms import ContactForm
+import asyncio
+from telegram import Bot
+from telegram.ext import Updater, MessageHandler
+from django.conf import settings
+loop = asyncio.get_event_loop()
+
+
 
 def index(request):
     context = {
@@ -6,9 +14,34 @@ def index(request):
     }
     return render(request, "main/index.html", context)
 
-
 def call(request):
-    return render(request, 'main/call.html',)
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            phone_number = form.cleaned_data['phone_number']
+            # Отправка данных в Telegram
+            message = f'Name: {name}\nPhone Number: {phone_number}'
+
+            async def send_telegram_message():
+                # Замените на ваш токен бота
+                bot = Bot(token='6160065485:AAFJ7j6U7xApViYEDhBpEB2iCIEOo8vB3Bc')
+
+                # Замените на ваш Chat ID
+                chat_id = '-1001920393262'
+
+                # Текст сообщения
+
+                # Отправка сообщения
+                await bot.send_message(chat_id=chat_id, text=message)
+            loop.run_until_complete(send_telegram_message())
+
+            return render(request, 'main/index.html')
+        else:
+            return render(request, 'main/call.html', {'form': form})
+    else:
+        form = ContactForm()
+        return render(request, 'main/call.html', {'form': form})
 
 def show_apartment(request):
     return render(request, 'main/show_apartment.html', )
